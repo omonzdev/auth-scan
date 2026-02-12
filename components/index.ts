32ae58058301ac74e266d8fe71e71543671fa9d9
omonzdev <omonzemmydev@gmail.com>
2026-02-12 13:07:11 +0100

    removed unused imports

--- components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Contestant } from "./Contestant";
-export { default as Footer } from "./Footer";
-export { default as Hero } from "./Hero";
-export { default as Navbar } from "./Navbar";
-export { default as Sponsor } from "./Sponsors";
